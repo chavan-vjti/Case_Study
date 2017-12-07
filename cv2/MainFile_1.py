8ae229704135fecc7e01c8480f51caa20d2b5045
Siddhesh Chavan <chavan.vjti@gmail.com>
2017-12-07 19:58:29 +0530

    Update: Green line on hour hand for 1.jpg

--- cv2/MainFile_1.py
+++ cv2/MainFile_1.py
@@ -4,13 +4,14 @@ from matplotlib import pyplot
 import numpy as np
 
 
-img = cv2.imread('1.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('1.jpg')
 
 
 # cv2.imshow('Picture',img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+cv2.line(img,(102,109),(83,96),(0,255,0),3)
 pyplot.imshow(img, cmap='gray', interpolation='bicubic')
 # pyplot.plot([84,96],[103,109],'b',linewidth = 5)
 pyplot.show()
