035a62f0be336d65051ea4e553a86681506c496d
Siddhesh Chavan <chavan.vjti@gmail.com>
2017-12-07 20:06:41 +0530

    Update: Created 1_New.jpg

--- cv2/MainFile_1.py
+++ cv2/MainFile_1.py
@@ -6,13 +6,16 @@ import numpy as np
 
 img = cv2.imread('1.jpg')
 
-
-# cv2.imshow('Picture',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
 cv2.line(img,(102,109),(83,96),(0,255,0),3)
-pyplot.imshow(img, cmap='gray', interpolation='bicubic')
+cv2.circle(img,(102,109,),55,(255,0,0), 1) #It works like RGB
+cv2.circle(img,(102,109,),10,(0,0,255), 1)
+cv2.imshow('Picture',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('1_New.jpg', img)
+
+
+# pyplot.imshow(img, cmap='gray', interpolation='bicubic')
 # pyplot.plot([84,96],[103,109],'b',linewidth = 5)
-pyplot.show()
+# pyplot.show()
 
