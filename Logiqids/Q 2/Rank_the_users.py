43644ebf1a13361ba43ac3808c15f41e4d3a8d58
S_Chavan <chavan.vjti@gmail.com>
2018-02-21 23:23:22 +0530

    Update

--- Logiqids/Q 2/Rank_the_users.py
+++ Logiqids/Q 2/Rank_the_users.py
@@ -1,7 +1,7 @@
 import csv
 int_list = []
 final_list = []
-with open('Demo.csv','rb') as f:
+with open('marks_list.csv','rb') as f:
     reader = csv.reader(f)
     next(reader, None)
     for row in reader:
@@ -9,4 +9,13 @@ with open('Demo.csv','rb') as f:
 
 for i in int_list:
     final_list.append(i[1])
+
+topper = max(final_list)
+
+for itr in range(len(final_list)):
+    if final_list[itr] == topper:
+        final_list[itr] = 1
+    else:
+        topper = topper - 1
+        print topper
 print final_list
